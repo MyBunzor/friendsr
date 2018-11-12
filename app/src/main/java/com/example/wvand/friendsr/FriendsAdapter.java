4f396f2158754dc73633a3d9ebb4ecffc959016c
Wim <44021592+MyBunzor@users.noreply.github.com>
2018-11-12 14:08:47 +0100

    second commit

---
+++ app/src/main/java/com/example/wvand/friendsr/FriendsAdapter.java
@@ -0,0 +1,51 @@
+package com.example.wvand.friendsr;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsAdapter extends ArrayAdapter<Friend> {
+
+    // Declaring friends as an arraylist, so we can fill it with objects in constructor below
+    ArrayList<Friend> friends;
+
+    public FriendsAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Friend> objects) {
+        super(context, resource, objects);
+        this.friends = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+
+        // Get access to layout's views
+        ImageView friendimage = convertView.findViewById(R.id.friendimage);
+        TextView friendname = convertView.findViewById(R.id.friendname);
+        TextView friendbio = convertView.findViewById(R.id.friendbio);
+
+        // Getting data from friends list
+        int drawable = friends.get(position).getDrawableId();
+        String name = friends.get(position).getName();
+        String bio = friends.get(position).getBio();
+
+        // Use data to fill the layout's views
+        friendname.setText(name);
+        friendbio.setText(bio);
+        friendimage.setImageResource(drawable);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/wvand/friendsr/MainActivity.java
+++ app/src/main/java/com/example/wvand/friendsr/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wvand.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Friend> friends = new ArrayList<>();
+        friends = new ArrayList<>();
+
+        // Adding the drawable id
+        int drawableIdarya = this.getResources().getIdentifier("arya", "id", this.getPackageName());
+        int drawableIdcersei = this.getResources().getIdentifier("cersei", "id", this.getPackageName());
+        int drawableIddaenerys = this.getResources().getIdentifier("daenerys", "id", this.getPackageName());
+        int drawableIdjaime = this.getResources().getIdentifier("jaime", "id", this.getPackageName());
+        int drawableIdjon = this.getResources().getIdentifier("jon", "id", this.getPackageName());
+        int drawableIdjorah = this.getResources().getIdentifier("jorah", "id", this.getPackageName());
+        int drawableIdmargaery = this.getResources().getIdentifier("margaery", "id", this.getPackageName());
+        int drawableIdmelisandre = this.getResources().getIdentifier("melisandre", "id", this.getPackageName());
+        int drawableIdsansa = this.getResources().getIdentifier("sansa", "id", this.getPackageName());
+        int drawableIdtyrion = this.getResources().getIdentifier("tyrion", "id", this.getPackageName());
+
+        // Instantiate Friend objects
+        Friend arya = new Friend("arya", "This is arya.", drawableIdarya);
+        Friend cersei = new Friend("cersei", "This is cersei", drawableIdcersei);
+        Friend daenerys = new Friend("daenerys", "This is daenerys", drawableIddaenerys);
+        Friend jaime = new Friend("jaime", "This is jaime", drawableIdjaime);
+        Friend jon = new Friend("jon", "This is jon", drawableIdjon);
+        Friend jorah = new Friend("jorah", "This is jorah", drawableIdjorah);
+        Friend margaery = new Friend("margaery", "This is margaery", drawableIdmargaery);
+        Friend melisandre = new Friend("melisandre", "this is melisandre", drawableIdmelisandre);
+        Friend sansa = new Friend("sansa", "This is sansa", drawableIdsansa);
+        Friend tyrion = new Friend("tyrion", "This is tyrion", drawableIdtyrion);
+
+        // Adding friend instantions to friends
+        friends.add(arya);
+        friends.add(cersei);
+        friends.add(daenerys);
+        friends.add(jaime);
+        friends.add(jon);
+        friends.add(jorah);
+        friends.add(margaery);
+        friends.add(melisandre);
+        friends.add(sansa);
+        friends.add(tyrion);
     }
+
 }
